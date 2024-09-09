176bb53838a02763332cde8834ea79c2bdb91c3d
shahriar <shahriarksaurov@gmail.com>
2024-09-10 01:13:45 +0200

    initial commit

---
+++ src/sections/Header.tsx
@@ -0,0 +1,18 @@
+export const HeaderSection = () => {
+    return (
+        <div>
+            <div className="continer">
+                <div className="flex justify-between">
+                    {/*left side */}
+                    <div>
+                        <img src="/assets/images/logo.png" alt="CarebroX" style={{width:"30%"}}/>
+                    </div>
+                    {/*right side */}
+                    <div>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+    }
