f5670ebbcf6e25f1ebb411e32149c7385c7a41e5
shahriar <shahriarksaurov@gmail.com>
2024-09-14 02:56:46 +0200

    hero animation

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,12 +1,27 @@
+import { motion } from "framer-motion";
 import type { ComponentPropsWithoutRef } from "react"
 import { twMerge } from "tailwind-merge";
 
-export const Circle = (props:ComponentPropsWithoutRef<'div'>) => {
-    const {className, children} = props;
+
+export const Circle = (props:ComponentPropsWithoutRef<'div'> & {animate?:boolean}) => {
+    const {className, children, animate = false} = props;
   return (
     <div 
-        className={twMerge("bg-[#202733] size-[240px] inline-flex items-center justify-center rounded-full outline outline-[6px] -outline-offset-[6px] outline-pink-500/10",className)}
+        className={twMerge("bg-[#202733] relative size-[240px] inline-flex items-center justify-center rounded-full",className)}
     >
+      <motion.div 
+        className={twMerge("absolute inset-0 rounded-full outline outline-[6px] -outline-offset-[6px] outline-pink-500/10",
+          animate && "border-[6px] border-transparent border-t-pink-500/30"
+        )}
+        animate={animate &&{
+          rotate: 360,
+        }}
+        transition={{
+          duration:15,
+          repeat: Infinity,
+          ease: "linear"
+        }}
+      />
         {children}
     </div>
   )
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,9 +1,39 @@
+import { useRef } from "react"
 import { Circle } from "../components/Circle"
 import { CutCornerButton } from "../components/CutCornerButton"
 import { Hexagone } from "../components/Hexagone"
-
+import {motion, useScroll, useTransform} from "framer-motion"
 
 export const HeroSection = () => {
+    const icosahedronRef = useRef<HTMLImageElement>(null)
+    const cubeRef = useRef<HTMLImageElement>(null)
+    const torusRef = useRef<HTMLImageElement>(null)
+    const cuboidRef = useRef<HTMLImageElement>(null)
+
+    const {scrollYProgress:scrollYProgressCuboid}=useScroll({
+        target: cuboidRef,
+        offset:['start end', 'end start'],
+    });
+
+    const {scrollYProgress:scrollYProgressTorus}=useScroll({
+        target: torusRef,
+        offset:['start end', 'end start'],
+    });
+
+    const {scrollYProgress:scrollYProgressCube}=useScroll({
+        target: cubeRef,
+        offset:['start end', 'end start'],
+    });
+
+    const {scrollYProgress}=useScroll({
+        target: icosahedronRef,
+        offset:['start end', 'end start'],
+    });
+
+    const icosahedronRotate = useTransform(scrollYProgress, [0, 1], [30, -45]);
+    const cubeRotate = useTransform(scrollYProgressCube, [0, 1], [100, -45]);
+    const torusRotate = useTransform(scrollYProgressTorus, [0, 1], [20, -20]);
+    const cuboidRotate = useTransform(scrollYProgressCuboid, [0, 1], [20, -20]);
     return (
         <section className="py-24 md:py-52 overflow-x-clip">
             <div className="container">
@@ -24,45 +54,88 @@ export const HeroSection = () => {
                 <div className="flex justify-center mt-24">
                     <div className="inline-flex relative z-0">
                         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                            <Hexagone className="size-[1100px]"/>
+                            <Hexagone className="size-[1100px]" size={1100}/>
                         </div>
                         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                            <Hexagone className="size-[1800px]"/>
+                            <Hexagone className="size-[1800px]" size={1800} reverse duration={60}/>
                         </div>
                         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                            <Circle className="absolute left-[200px] -top-[900px]">
-                                <img src="/assets/images/cube.png" alt="Cube" className="size-[140px]"/>
+                            <Circle className="absolute left-[200px] -top-[900px]" animate>
+                                <motion.img 
+                                    src="/assets/images/cube.png" 
+                                    alt="Cube" 
+                                    className="size-[140px]"
+                                    ref={cubeRef}
+                                    style={{
+                                        rotate: cubeRotate
+                                    }}
+                                />
                             </Circle>
                         </div>
 
                         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                            <Circle className="absolute left-[200px] top-[270px]">
-                                <img src="/assets/images/cuboid.png" alt="Cuboid" className="size-[140px]"/>
+                            <Circle className="absolute left-[200px] top-[270px]" animate>
+                                <motion.img 
+                                    src="/assets/images/cuboid.png" 
+                                    alt="Cuboid" 
+                                    className="size-[140px]"
+                                    ref={cuboidRef}
+                                    style={{
+                                        rotate: cuboidRotate
+                                    }}
+                                />
                             </Circle>
                         </div>
 
                         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
                             <Circle className="absolute -left-[600px] -top[80px]">
-                                <img src="/assets/images/torus.png" alt="Torus" className="size-[140px]"/>
+                                <motion.img 
+                                    src="/assets/images/torus.png"
+                                    alt="Torus" 
+                                    className="size-[140px]"
+                                    ref={torusRef}
+                                    style={{
+                                        rotate: torusRotate
+                                    }}
+                                    />
                             </Circle>
                         </div>
 
-                        <img 
-                            src="/assets/images/icosahedron.png" 
-                            alt="" 
-                            className="absolute w-[calc(100%+100px)] max-w-none -z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 saturate-[10%] brightness-[4%] hue-rotate-[240deg]"
-                        />
-                        <img 
-                            src="/assets/images/icosahedron.png"
-                            alt="Icosahedron as 3D image" 
-                            className="w-[500px]"
-                        />
+                        <motion.div className="inline-flex" ref={icosahedronRef} style={{
+                            rotate: icosahedronRotate
+                        }}>
+                            <img 
+                                src="/assets/images/icosahedron.png" 
+                                alt="" 
+                                className="absolute w-[calc(100%+100px)] max-w-none -z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 saturate-[10%] brightness-[4%] hue-rotate-[240deg]"
+                            />
+                            <img 
+                                src="/assets/images/icosahedron.png"
+                                alt="Icosahedron as 3D image" 
+                                className="w-[500px]"
+                            />
+                        </motion.div>
+                        
                     </div>
                 </div>
 
                 <div className="flex justify-center flex-col items-center mt-40 md:mt-80 gap-4">
                     <div className="h-10 w-5 outline outline-[6px] outline-pink-500/10 inline-flex justify-center pt-2 rounded-full">
-                        <div className="h-3 w-1 rounded-full bg-pink-500"></div>
+                        <motion.div 
+                            className="h-3 w-1 rounded-full bg-pink-500"
+                            animate={{
+                                translateY:12,
+                                opacity:0.2
+                            }}
+                            transition={{
+                                repeat: Infinity,
+                                duration: 2,
+                                ease: "linear",
+                                repeatType:"loop"
+                            }}
+                        >
+
+                            </motion.div>
                     </div>
                     <p className="uppercase text-zinc-500 font-extrabold tracking-wider">Scroll to learn more</p>
                 </div>
