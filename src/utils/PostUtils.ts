c177fc2c96f8f03cef78611a0224dc48ad2d6170
shahriar <shahriarksaurov@gmail.com>
2024-09-15 22:46:20 +0200

    career  page

--- src/utils/PostUtils.ts
+++ src/utils/PostUtils.ts
@@ -10,3 +10,29 @@ export const getPostColorFromCategory = (category:string) => {
             return 'pink'
     }
 }
+
+export const getPostColorFromType = (type:string) => {
+    switch (type) {
+        case 'Full Time':
+            return 'cyan'
+        case 'Part Time':
+            return 'violet'
+        case 'Contract':
+            return 'lime'
+        default:
+            return 'pink'
+    }
+}
+
+export const getColorFromPositionCategory = (category:string) => {
+    switch (category) {
+        case 'Engineering':
+            return 'violet'
+        case 'Product':
+            return 'lime'
+        case 'Marketing':
+            return 'cyan'
+        default:
+            return 'pink'
+    }
+}
